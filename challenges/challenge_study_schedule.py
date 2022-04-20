a3bf6da77bd97736b5afa369f96aeb72f2a0e3a9
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-20 17:56:38 -0300

    :bug: Fix: Resolve flake errors

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,9 +2,8 @@ def study_schedule(permanence_period, target_time):
     try:
         count = 0
         for person in permanence_period:
-            if  person[0] <= target_time <= person[1]:
+            if person[0] <= target_time <= person[1]:
                 count += 1
-        #study_schedule(permanence_period, target_time - 1)
         return count
     except (ValueError, TypeError):
         return None
