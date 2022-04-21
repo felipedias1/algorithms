978ce5316c3e073db544f61ebf1217dfe91dbf2d
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-21 15:55:25 -0300

    :hammer: Refact: Change find_duplicate function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,19 +1,21 @@
 from collections import Counter
-from typing import Type
 
 
 def find_duplicate(nums):
-    try:
-        all_nums = dict(Counter(nums))
-        print(all_nums)
-        duplicated = ""
-        for number, quantity in all_nums.items():
-            if number < 0:
-                return False
-            if quantity > 1:
-                duplicated = number
-        if not duplicated:
-            return False
-        return duplicated
-    except(TypeError, ValueError):
+    if check_string(nums) is False:
         return False
+    all_nums = dict(Counter(nums))
+    print(all_nums)
+    duplicated = ""
+    for key, value in all_nums.items():
+        if key < 0:
+            return False
+        if value > 1:
+            duplicated = key
+    return False if not duplicated else duplicated
+
+
+def check_string(list):
+    for num in list:
+        if type(num) == str:
+            return False
