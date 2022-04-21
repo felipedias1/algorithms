8460094f576008f9a95df27b514e553fbc036d3e
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-21 01:00:31 -0300

    :sparkles: Feat: Create recursive function to check if word is palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    elif word[high_index] != word[low_index]:
+        return False
+    elif high_index <= low_index:
+        return True
+    return is_palindrome_recursive(word, low_index+1, high_index-1)
