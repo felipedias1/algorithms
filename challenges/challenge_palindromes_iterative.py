05e256f79c9df0c2abe7808275cb97c7ecdbbe2c
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-21 17:04:37 -0300

    :hammer: Refact: Changes to clear code of is_palindrome_iterative function

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,8 +1,4 @@
 def is_palindrome_iterative(word):
     if word == "":
         return False
-    reverse_list = []
-    for letter in word:
-        reverse_list.insert(0, letter)
-    reverse_word = "".join(reverse_list)
-    return True if word == reverse_word else False
+    return True if word == word[::-1] else False
